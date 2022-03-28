d443e257ef614da264f5bd4f8f1312d92894df73
Scott Carpenter <carpenter.scott22@gmail.com>
2022-03-28 14:52:10 -0500

    first commit and creation of select_exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,2 +1,13 @@
 USE scott;
-
+/*Albums made by Pink Floyd*/
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+/*Year that Sgt. Pepper's Lonely Hearts Band album released*/
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Band';
+/*Genre of the album Nevermind*/
+SELECT genre FROM albums WHERE name = 'Nevermind';
+/*All albums produced in the 1990's*/
+SELECT * FROM albums WHERE release_date BETWEEN 1989 AND 2000;
+/*All albums that sold less than 20 million copies*/
+SELECT * FROM albums WHERE sales < 20;
+/*All albums with the Rock genre*/
+SELECT * FROM albums WHERE genre = 'Rock';
