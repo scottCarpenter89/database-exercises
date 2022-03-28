197a722cfb34709dccb3337c6dfc580761b1ffee
Scott Carpenter <carpenter.scott22@gmail.com>
2022-03-28 15:17:38 -0500

    added update exercises

--- update-exercises.sql
+++ update-exercises.sql
@@ -3,3 +3,14 @@ USE scott;
 SELECT name FROM albums;
 SELECT name FROM albums WHERE release_date < 1980;
 SELECT name FROM albums WHERE artist = 'Michael Jackson';
+
+UPDATE albums
+SET sales = sales * 10;
+
+UPDATE albums
+SET release_date = release_date - 100
+WHERE release_date < 1980;
+
+UPDATE albums
+SET artist = "Peter Jackson"
+WHERE artist = "Michael Jackson";
