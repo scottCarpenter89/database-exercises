7de9c45293575a112b33b42c90867e045d0f69ba
Scott Carpenter <carpenter.scott22@gmail.com>
2022-03-29 14:00:25 -0500

    completed exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -5,6 +5,28 @@ FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY first_name ASC, last_name ASC;
 
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name ASC;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE '%e%'
+ORDER BY emp_no ASC;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE '%e%'
+ORDER BY emp_no DESC;
+
+########################################################################################################################
+#The following was copied over the entire contents from where_exercises.sql
+
+SELECT first_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
 SELECT *
 FROM employees
 WHERE last_name LIKE 'E%';
