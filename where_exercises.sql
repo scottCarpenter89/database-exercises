831925892754ef33ef752c1466a6dcb58f2b1de4
Scott Carpenter <carpenter.scott22@gmail.com>
2022-03-29 12:02:15 -0500

    completed 1st exercise of part 2

--- where_exercises.sql
+++ where_exercises.sql
@@ -11,3 +11,8 @@ WHERE last_name LIKE 'E%';
 SELECT *
 FROM employees
 WHERE last_name LIKE  '%q%';
+
+SELECT first_name
+FROM employees
+WHERE first_name = 'Irena' OR first_name = 'Vidya'
+   OR first_name = 'Maya';
