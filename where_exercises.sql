baccf1bced66692e154df5c2d55a7b9a425a7ddc
Scott Carpenter <carpenter.scott22@gmail.com>
2022-03-29 12:15:56 -0500

    completed 4th exercise of part 2

--- where_exercises.sql
+++ where_exercises.sql
@@ -24,3 +24,7 @@ WHERE gender = 'm' AND (first_name IN ('Irena', 'Vidya', 'Maya'));
 SELECT *
 FROM employees
 WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
