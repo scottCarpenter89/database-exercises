a4904fee1f2258d30c510d3b1cdbde00e77e3838
Scott Carpenter <carpenter.scott22@gmail.com>
2022-03-29 14:46:50 -0500

    finished limit_exercises.sql

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -5,3 +5,16 @@ FROM employees
 ORDER BY last_name DESC
 LIMIT 10;
 
+#Adding extra salary field to validate results of first five results
+SELECT emp_no, salary
+FROM salaries
+ORDER BY salary DESC
+LIMIT 5;
+
+#Adding extra salary field to validate results of 10th 'page' @ 5 results per page
+SELECT emp_no, salary
+FROM salaries
+ORDER BY salary DESC
+LIMIT 5 OFFSET 46;
+
+
