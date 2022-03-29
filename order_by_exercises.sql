38eca59950de1af8319deff31fd9bd8badec91b6
Scott Carpenter <carpenter.scott22@gmail.com>
2022-03-29 13:45:20 -0500

    completed first three exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,35 @@
+USE employees;
+
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name ASC, last_name ASC;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE  '%q%';
+
+SELECT first_name
+FROM employees
+WHERE first_name = 'Irena' OR first_name = 'Vidya'
+   OR first_name = 'Maya';
+
+SELECT first_name
+FROM employees
+WHERE gender = 'm' AND (first_name IN ('Irena', 'Vidya', 'Maya'));
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
