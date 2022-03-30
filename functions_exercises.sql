eaf17ef7d1ee58a33405fb6715bf8ee9bf31db28
Scott Carpenter <carpenter.scott22@gmail.com>
2022-03-29 22:34:15 -0500

    completed exercise 4 on functions_exercises.sql

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -4,9 +4,13 @@ SELECT CONCAT(first_name, ' ', last_name) AS 'Last name sandwiched by E''s'
 FROM employees
 WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 
-SELECT CONCAT(first_name, ' ', last_name), birth_date AS 'People born on X-mas'
+SELECT CONCAT(first_name, ' ', last_name) AS 'People born on X-mas', birth_date
 FROM employees
 WHERE (MONTH(birth_date) = 12) AND (DAY(birth_date) = 25);
+
+SELECT CONCAT(first_name, ' ', last_name) AS 'People born on X-mas and hired in the 90''s', birth_date, hire_date
+FROM employees
+WHERE (MONTH(birth_date) = 12) AND (DAY(birth_date) = 25) AND (YEAR(hire_date) BETWEEN 1990 AND 1999);
 
 
 ############################################################################################################
