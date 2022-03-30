148f690e7d71c8075980542efdd3322ac7a170b2
Scott Carpenter <carpenter.scott22@gmail.com>
2022-03-30 11:43:54 -0500

    completed group_by_exercises.sql exercises

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -8,7 +8,15 @@ FROM employees
 WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
 GROUP BY first_name, last_name;
 
-SELECT last_name
+SELECT LTRIM(COUNT(last_name)) AS 'Unique Name Count', last_name
 FROM employees
 WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%'
 GROUP BY last_name;
+
+SELECT gender, COUNT(gender)
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
+
+
+
