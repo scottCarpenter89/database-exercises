dc1f558328488fc642e8027442805c7df3544b86
Scott Carpenter <carpenter.scott22@gmail.com>
2022-03-30 11:29:14 -0500

    exercise 5 of group_by_exercises.sql done

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -8,3 +8,7 @@ FROM employees
 WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
 GROUP BY first_name, last_name;
 
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%'
+GROUP BY last_name;
