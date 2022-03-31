1c110b6b90df669b42ff26e8f9839b0577279f9f
Scott Carpenter <carpenter.scott22@gmail.com>
2022-03-31 16:33:35 -0500

    completed all female managers exercise in join_exercises.sql

--- join_exercises.sql
+++ join_exercises.sql
@@ -10,4 +10,12 @@ ON d.dept_no = dm.dept_no
 WHERE dm.to_date = '9999-01-01';
 #WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
 
+#SELECT all of the departments currently managed by women
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM employees AS e
+JOIN dept_manager AS dm
+ON dm.emp_no = e.emp_no
+JOIN departments AS d
+ON d.dept_no = dm.dept_no
+WHERE e.gender = 'F' AND dm.to_date = '9999-01-01';
 
