01a10485f317be9b40a9ad4fc915811a1c9f79d1
Scott Carpenter <carpenter.scott22@gmail.com>
2022-03-31 08:26:13 -0500

    completed aliases_exercises.sql

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,5 +1,5 @@
 USE scott;
 
-SELECT CONCAT(last_name, ', ', first_name) AS full_name, birth_date AS DOB
+SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name) AS full_name, birth_date AS DOB
 FROM employees
 LIMIT 10;
