ddbdae1dbefda78b56d2d4b47274ad6c9e74ede6
Scott Carpenter <carpenter.scott22@gmail.com>
2022-03-31 16:25:48 -0500

    created associative databases successfully

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,2 +1,13 @@
 USE scott;
 
+#We need to show the department name and the department manager
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM employees AS e
+JOIN dept_manager AS dm
+ON dm.emp_no = e.emp_no
+JOIN departments AS d
+ON d.dept_no = dm.dept_no
+WHERE dm.to_date = '9999-01-01';
+#WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
+
+
