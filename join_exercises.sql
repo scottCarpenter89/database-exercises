6a4043c23c54317030dc8868d2f2f795465e1e01
Scott Carpenter <carpenter.scott22@gmail.com>
2022-04-01 10:40:13 -0500

    completed add exercises in join_exercises.sql

--- join_exercises.sql
+++ join_exercises.sql
@@ -19,3 +19,36 @@ JOIN departments AS d
 ON d.dept_no = dm.dept_no
 WHERE e.gender = 'F' AND dm.to_date = '9999-01-01';
 
+#We're SELECTing all employees in Customer Service with a specific title and counting the instances
+SELECT t.title AS 'Job Title', COUNT(t.emp_no) AS 'Total People w/ Job Title'
+FROM employees as e
+JOIN dept_emp AS de
+ON de.emp_no = e.emp_no
+JOIN titles AS t
+ON t.emp_no = e.emp_no
+JOIN departments AS d
+ON d.dept_no = de.dept_no
+WHERE d.dept_no = 'd009' AND t.to_date > CURDATE()
+GROUP BY t.title
+ORDER BY COUNT(t.emp_no) DESC;
+
+#We are selecting the department name, department manager, and their salary ordering by their department name
+SELECT DISTINCT d.dept_name AS 'Department Name',
+       CONCAT(e.first_name, ' ', e.last_name) as 'Department Manager',
+       s.salary AS Salary
+FROM employees AS e
+JOIN dept_manager AS dm
+ON dm.emp_no = e.emp_no
+JOIN departments AS d
+ON d.dept_no = dm.dept_no
+JOIN salaries AS s
+ON s.emp_no = e.emp_no
+WHERE s.to_date > CURDATE() AND dm.to_date > CURDATE();
+
+
+
+
+
+
+
+
